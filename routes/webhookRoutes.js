bdd19ed4a80ade80cf0bfd2c381e66fca71517d9
hamzasabermahmoud <hamzasabermahmoud@gmail.com>
2024-10-01 16:13:24 +0300

    Initial commit

---
+++ routes/webhookRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const orderController = require("../controllers/orderController");
+
+const router = express.Router();
+
+router.route("/webhook").post(orderController.webhook);
+
+module.exports = router;
