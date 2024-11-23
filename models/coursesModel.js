9c85134589c0a4c7a5bd5689d52e83ff15d83bdf
hamzasabermahmoud <hamzasabermahmoud@gmail.com>
2024-11-23 07:27:10 +0200

    intro-vid

--- models/coursesModel.js
+++ models/coursesModel.js
@@ -12,6 +12,34 @@ const videoSchema = new mongoose.Schema({
     type: String,
     required: [true, "A video must have a file path"],
   },
+  watched: {
+    type: Boolean,
+    default: false,
+  },
+});
+const questionsSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: [true, "a quiz must have a question"],
+  },
+  rightAnswer: {
+    type: String,
+    required: [true, "a question must has an anwer "],
+  },
+  answers: [
+    {
+      type: String,
+      required: [true, "a question must has an array of anwers "],
+    },
+  ],
+});
+const quizSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: [true, "a quiz must have a title"],
+  },
+  questions: [questionsSchema],
+  grade: Number,
 });
 
 // Define the chapter subdocument schema
@@ -20,6 +48,7 @@ const chapterSchema = new mongoose.Schema({
     type: String,
     required: [true, "A chapter must have a title"],
   },
+  quiz: [quizSchema],
   videos: [videoSchema], // Embed video schema as an array
 });
 
@@ -32,6 +61,13 @@ const courseSchema = new mongoose.Schema({
   description: String,
   introVideo: String, // Path to the intro video
   chapters: [chapterSchema], // Embed chapter schema as an array
+  level: {
+    type: String,
+    required: [true, "A course must have a level"],
+  },
+  resources: {
+    type: String,
+  },
   instructors: [
     {
       type: mongoose.Schema.ObjectId,
